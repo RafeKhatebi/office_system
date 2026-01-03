f043d1d21b6bfb8f90f587cc8a74b784a9faa70e
12mousavi1400-creator <12mousavi1400@gmail.com>
2026-01-03 10:05:17 +0430

    Update EmployeeController.php

--- app/Http/Controllers/Api/EmployeeController.php
+++ app/Http/Controllers/Api/EmployeeController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Validator;
 
+use function Pest\Laravel\json;
+
 class EmployeeController extends Controller
 {
     /**
@@ -31,7 +33,6 @@ class EmployeeController extends Controller
             'first_name'   => 'required|string|max:255',
             'last_name'    => 'required|string|max:255',
             'job_title'    => 'required|string|max:255',
-            'hire_date'    => 'required|date|before_or_equal:today',
             'phone'        => 'required|string|max:15',
             'emergency_phone' => 'nullable|string|max:15',
             'gender'          => 'nullable|in:male,female',
@@ -45,7 +46,7 @@ class EmployeeController extends Controller
             return response()->json([
                 'message' => 'Validation error',
                 'errors'  => $validator->errors(),
-            ]);
+            ], 422);
         }
         // save employee's information
         $employee = employee::create([
@@ -71,7 +72,18 @@ class EmployeeController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $employee = employee::find($id);
+        if (!$employee) {
+            return response()->json([
+                'message'  => 'Data not found to This id: ' .$id,
+                'data'     => $employee
+
+            ], 404);
+        }
+        return response()->json([
+            'message'  => 'Data fetched successfully',
+            'employee' => $employee
+        ]);
     }
 
     /**
@@ -79,7 +91,42 @@ class EmployeeController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $employee = employee::find($id);
+        $validator = Validator::make($request->all(), [
+            'first_name'   => 'required|string|max:255',
+            'last_name'    => 'required|string|max:255',
+            'job_title'    => 'required|string|max:255',
+            'phone'        => 'required|string|max:15',
+            'emergency_phone' => 'nullable|string|max:15',
+            'gender'          => 'nullable|in:male,female',
+            'date_of_birth'   => 'nullable|date|before_or_equal:today',
+            'national_id'     => 'nullable|string|max:255|unique:employees,national_id,'.$id,
+            'status'          => 'nullable|in:active,inactive',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message'  => 'Validation error',
+                'errors'   => $validator->errors(),
+            ]);
+        }
+
+        $employee->update([
+            'first_name'  => $request->first_name,
+            'last_name'   => $request->last_name,
+            'job_title'   => $request->job_title,
+            'phone'       => $request->phone,
+            'emergency_phone' => $request->emergency_phone,
+            'gender'          => $request->gender,
+            'date_of_birth'   => $request->date_of_birth,
+            'national_id'     => $request->national_id,
+            'status'          => $request->status
+        ]);
+
+        return response()->json([
+            'message'  => 'Data updated successfully',
+            'data'     => $employee
+        ]);
     }
 
     /**
@@ -87,6 +134,17 @@ class EmployeeController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $employee = employee::find($id);
+        if (!$employee) {
+            return response()->json([
+                'message'  => 'Not found id: '.$id,
+            ], 404);
+        }
+
+        $employee->delete();
+        return response()->json([
+            'message'  => 'Data deleted successfully',
+        ]);
+
     }
 }
