f60967a7f9a6c7b40e62762c4288cf729ee89f28
Rafe Khatebi <rkhatibi2003@gmail.com>
2026-01-03 11:43:23 +0430

    added the structure of layout and tailwind for style

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,5 +1,9 @@
 import { createInertiaApp } from "@inertiajs/react";
 import { createRoot } from "react-dom/client";
+import './bootstrap'; // Uncomment if you have bootstrap or other initializations
+import '../css/app.css'; // Uncomment if you have global CSS
+// import css styles or other assets as needed
+import '../../public/assets/styles/tailwind.css';
 createInertiaApp({
     resolve: name => {
         const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true });
@@ -8,4 +12,5 @@ createInertiaApp({
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
     },
+    
 });
